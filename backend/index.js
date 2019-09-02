2bb9fae3a08e55a140a6cc2b1e7d696abbf6ae36
Francisco Sierra <franjrsierra18@gmail.com>
2019-09-02 14:02:33 +0200

    Esta montado el servidor y devuelve las respuestas

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.get('/', (req, res) => res.send('Hola express!'));
 
-app.listen(3000);
+app.listen(3000, () => console.log('servidor levantado'));
--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,1 +1,8 @@
-const respuesta = document.querySelector('.res');
+fetch('http://localhost:3000')
+.then(res => res.text())
+.then(res => {
+  document.querySelector('.res').innerHTML = `Esto es lo que devuelve la peticion = ${res}
+  <h1>Bienvenidos</h1>`;
+})
+.catch(err => console.log(err))
+
