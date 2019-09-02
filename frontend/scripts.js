06694fc0f968c1b3a083c2c721bfcd21aaa17fc6
Francisco Sierra <franjrsierra18@gmail.com>
2019-09-02 14:13:37 +0200

    agregado boton que hace la peticion

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,8 +1,12 @@
-fetch('http://localhost:3000')
-.then(res => res.text())
-.then(res => {
-  document.querySelector('.res').innerHTML = `Esto es lo que devuelve la peticion = ${res}
-  <h1>Bienvenidos</h1>`;
-})
-.catch(err => console.log(err))
+function botonAPI() {
+  fetch('http://localhost:3000')
+  .then(res => res.text())
+  .then(res => {
+    document.querySelector('.res').innerHTML = `Esto es lo que devuelve la peticion = ${res}
+    <h1>Bienvenidos</h1>`;
+  })
+  .catch(err => console.log(err))
+  
+}
+
 
